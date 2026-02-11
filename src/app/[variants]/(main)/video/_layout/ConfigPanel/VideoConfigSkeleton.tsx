50ca5be4a6c7c0de16a55f2f86e083a0679e3a7c
YuTengjing <ytj2713151713@gmail.com>
2026-02-11 21:19:39 +0800

    💄 style: improve video ConfigPanel skeleton to match full control layout

--- src/app/[variants]/(main)/video/_layout/ConfigPanel/VideoConfigSkeleton.tsx
+++ src/app/[variants]/(main)/video/_layout/ConfigPanel/VideoConfigSkeleton.tsx
@@ -5,11 +5,56 @@ import { memo } from 'react';
 
 const VideoConfigSkeleton = memo(() => {
   return (
-    <Flexbox gap={32} padding="12px 12px 0 12px" style={{ height: '100%' }}>
-      {/* Model Selection */}
+    <Flexbox gap={16} padding={10}>
+      {/* ModelSelect */}
+      <Skeleton.Button active size="large" style={{ width: '100%' }} />
+
+      {/* FrameUpload: imageUrl */}
       <Flexbox gap={8}>
-        <Skeleton.Button active size="small" style={{ width: 100 }} />
-        <Skeleton.Button active size="large" style={{ width: '100%' }} />
+        <Skeleton.Button active size="small" style={{ width: 60 }} />
+        <Skeleton.Block active style={{ borderRadius: 8, height: 120, width: '100%' }} />
+      </Flexbox>
+
+      {/* FrameUpload: endImageUrl */}
+      <Flexbox gap={8}>
+        <Skeleton.Button active size="small" style={{ width: 50 }} />
+        <Skeleton.Block active style={{ borderRadius: 8, height: 120, width: '100%' }} />
+      </Flexbox>
+
+      {/* AspectRatio */}
+      <Flexbox gap={8}>
+        <Skeleton.Button active size="small" style={{ width: 70 }} />
+        <Skeleton.Block active style={{ borderRadius: 8, height: 64, width: '100%' }} />
+      </Flexbox>
+
+      {/* Resolution */}
+      <Flexbox gap={8}>
+        <Skeleton.Button active size="small" style={{ width: 50 }} />
+        <Skeleton.Button active size="default" style={{ width: '100%' }} />
+      </Flexbox>
+
+      {/* Duration */}
+      <Flexbox gap={8}>
+        <Skeleton.Button active size="small" style={{ width: 40 }} />
+        <Skeleton.Button active size="default" style={{ width: '100%' }} />
+      </Flexbox>
+
+      {/* Seed */}
+      <Flexbox gap={8}>
+        <Skeleton.Button active size="small" style={{ width: 40 }} />
+        <Skeleton.Button active size="default" style={{ width: '100%' }} />
+      </Flexbox>
+
+      {/* generateAudio switch */}
+      <Flexbox align="center" horizontal justify="space-between">
+        <Skeleton.Button active size="small" style={{ width: 70 }} />
+        <Skeleton.Button active size="small" style={{ borderRadius: 12, width: 44 }} />
+      </Flexbox>
+
+      {/* cameraFixed switch */}
+      <Flexbox align="center" horizontal justify="space-between">
+        <Skeleton.Button active size="small" style={{ width: 70 }} />
+        <Skeleton.Button active size="small" style={{ borderRadius: 12, width: 44 }} />
       </Flexbox>
     </Flexbox>
   );
